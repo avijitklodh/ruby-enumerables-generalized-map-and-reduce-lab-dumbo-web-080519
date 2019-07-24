aaf28b1bf62ac74da8c3b9b24abc5ffa3fd4fd4c
Avijit Lodh <avijitklodh@gmail.com>
2019-07-24 22:38:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,3 +10,12 @@ def map(source_array)
   return new
 end
 
+def map_to_square(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( yield source_array[i]
+    i += 1
+  end
+  return new
+end

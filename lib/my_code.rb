12dc4be76c25ea233958d46fc5441dcb3af4c9f6
Avijit Lodh <avijitklodh@gmail.com>
2019-07-24 22:55:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,8 +13,14 @@ end
 def reduce(source_array, starting_point=nil)
   if starting_point
     value = starting_point
-    i =  
+    i =  1
   else 
-    
-  
+    starting_point = source_array[0]
+    i = 0
+  while i < source_array.length do
+    value = ( yield (source_array[i],value) 
+    i += 1
+  end
+  return new
+end
     

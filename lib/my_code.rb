5adec3d83dc283d5f2ee78debde07bf1519ef630
Avijit Lodh <avijitklodh@gmail.com>
2019-07-24 23:02:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,4 +11,17 @@ def map(source_array)
 end
 
 
-    
+    def reduce(s, sp=nil)
+  if sp
+    accum = sp
+    i = 0
+  else
+    accum = s[0]
+    i = 1
+  end
+  while i < s.length
+    accum = yield(accum, s[i])
+    i += 1
+  end
+  accum
+end
